b61830e24d5cf87d08dbdb52a141f0881a62e099
Muerdter, Nick <nick.muerdter@nrel.gov>
2010-12-01 00:30:31 +0000

    Adding a templated /etc/environment file.

---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: etc
+# Recipe:: default
+#
+# Copyright 2010, NREL
+#
+# All rights reserved - Do Not Redistribute
+#
---
+++ recipes/environment.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: etc
+# Recipe:: environment
+#
+# Copyright 2010, NREL
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template "/etc/environment" do
+  source "environment.erb"
+  mode 0644
+  owner "root"
+  group "root"
+end
